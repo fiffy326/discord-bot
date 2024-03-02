d965d1d95c9366090e2ea0e5e885db52cec1f762
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-03-02 16:49:14 +1000

    Added support for multiple relays

---
+++ src/base/interfaces/relay.ts
@@ -0,0 +1,8 @@
+export interface Relay {
+    webhookURL: string;
+    guildIds: Array<string>;
+    channelIds: Array<string>;
+    userIds: Array<string>;
+}
+
+export default Relay;
--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -1,26 +1,32 @@
 import config from "../../config.json" with { type: "json" };
-import { Events, Message, TextChannel, WebhookClient } from "discord.js";
+import Relay from "../base/interfaces/relay.js";
+import { cleanContent, Events, Message, TextChannel, WebhookClient } from "discord.js";
 
-const webhookClient = new WebhookClient({ url: config.relay.outputWebhookURL });
 const zeroWidthSpace = "​";
+const relays: Array<Relay> = config.relays;
 
 export default {
   name: Events.MessageCreate,
   once: false,
   async execute(message: Message) {
     if (message.author.bot || message.webhookId) return;
-    if (message.guildId != config.relay.inputGuildId) return;
-
-    const channelName = (<TextChannel>message.channel).name;
-    const username = message.author.username;
-    const content = (!message.content) ? zeroWidthSpace : message.cleanContent;
-
-    webhookClient.send({
-      username: `${username} [#${channelName}]`,
-      avatarURL: message.author.avatarURL()!,
-      content: content,
-      embeds: [...message.embeds.values()],
-      files: [...message.attachments.values()]
+    relays.forEach((relay: Relay) => {
+      if ((relay.guildIds.includes(message.guildId!.toString())) ||
+          (relay.channelIds.includes(message.channelId.toString())) ||
+          (relay.userIds.includes(message.author.id.toString()))) {
+        let webhookClient = new WebhookClient({ url: relay.webhookURL });
+        let channel = (<TextChannel>message.channel);
+        let channelName = channel.name;
+        let username = message.author.username;
+        let content = !message.content ? zeroWidthSpace : cleanContent(message.content, channel);
+        webhookClient.send({
+          content: content,
+          username: `${username} [#${channelName}]`,
+          avatarURL: message.author.avatarURL()!,
+          embeds: [...message.embeds.values()],
+          files: [...message.attachments.values()]
+        });
+      }
     });
   },
 };
