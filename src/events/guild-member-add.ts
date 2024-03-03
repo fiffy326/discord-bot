b0b17df954a28f1162257a3c56283e7a91568f7f
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-03-04 07:37:36 +1000

    Added new member welcome message to server

---
+++ src/events/guild-member-add.ts
@@ -0,0 +1,15 @@
+import config from "../utils/config.js";
+import Event from "../interfaces/event.js";
+import { Events, GuildMember, TextChannel } from "discord.js";
+
+export const event: Event = {
+  name: Events.GuildMemberAdd,
+  once: false,
+  async execute(member: GuildMember) {
+    const welcomeChannelId = config.bot.welcomeChannelId;
+    const welcomeChannel = await member.client.channels.fetch(welcomeChannelId);
+    (<TextChannel>welcomeChannel).send(`Welcome to the server, ${member}!`);
+  }
+};
+
+export default event;
