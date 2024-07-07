db7a05dccff11ec75ccc81b26031029dc8fda2b9
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-07-08 05:32:02 +1000

    Add client.ts
    Add class to represent the bot client.

---
+++ src/bot/client.ts
@@ -0,0 +1,54 @@
+import { Command, loadCommandFiles } from "./command.js";
+import { loadEventFiles } from "./event.js";
+import { Client as DiscordClient, Collection, GatewayIntentBits } from "discord.js";
+
+export class Client extends DiscordClient {
+  commands = new Collection<string, Command>();
+
+  constructor(intents?: GatewayIntentBits[]) {
+    super({
+      intents: intents ?? [
+        // Use all gateway intents by default.
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.AutoModerationExecution,
+        GatewayIntentBits.DirectMessagePolls,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessagePolls,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.GuildModeration,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildScheduledEvents,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.MessageContent,
+      ],
+    });
+  }
+
+  async loadCommands(): Promise<void> {
+    const commands = await loadCommandFiles();
+    for (const command of commands) {
+      this.commands.set(command.data.name, command);
+    }
+  }
+
+  async loadEvents(): Promise<void> {
+    const events = await loadEventFiles();
+    for (const event of events) {
+      if (event.once) {
+        this.once(event.name, (...args: unknown[]) => event.callback(...args));
+      } else {
+        this.on(event.name, (...args: unknown[]) => event.callback(...args));
+      }
+    }
+  }
+}
