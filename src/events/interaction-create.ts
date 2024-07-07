fee6991c5ac219a368a26af09440df72b73dd872
Emma Casey <22841956+fiffy326@users.noreply.github.com>
2024-07-08 05:34:20 +1000

    Add interaction-create.ts
    Add event handler for 'interactionCreate' to execute slash commands.

---
+++ src/events/interaction-create.ts
@@ -0,0 +1,27 @@
+import { Client } from "../bot/client.js";
+import { Event } from "../bot/event.js";
+import { log } from "../utils/log.js";
+import { Interaction } from "discord.js";
+
+export default {
+  name: "interactionCreate",
+  async callback(interaction: Interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = (interaction.client as Client).commands.get(interaction.commandName);
+    if (!command) {
+      log.error(`No command found: ${interaction.commandName}`);
+      return;
+    }
+
+    await command.callback(interaction).catch(async (e) => {
+      log.error(e.message);
+      const errorReply = "There was an error while executing this command.";
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: errorReply, ephemeral: true });
+      } else {
+        await interaction.reply({ content: errorReply, ephemeral: true });
+      }
+    });
+  },
+} as Event;
